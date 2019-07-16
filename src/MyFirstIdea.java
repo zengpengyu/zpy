364a2e0cc749ae41223fc59a7925c42ffb182a81
zengpengyu <2747459395@qq.com>
2019-07-16 15:24:31 +0800

    no

---
+++ src/MyFirstIdea.java
@@ -0,0 +1,12 @@
+public class MyFirstIdea {
+    public int a;
+    public int b;
+
+    MyFirstIdea() {
+    }
+
+    MyFirstIdea(int left, int right) {
+        a = left;
+        b = right;
+    }
+}
